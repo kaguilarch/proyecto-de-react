d971f036075cab012ad1fbd6d66754741231a342
kaguilarch <kaguilarch@outlook.com>
2023-04-21 23:49:37 -0600

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import CartWidget from "./Components/assets/CardWidget";
+
+
+
+function App () {
+  return(
+    <div className="container">
+      <NavBar/>
+      <CartWidget/>
+    </div>
+  )
+}
+ export default App;
---
+++ src/Components/assets/CardWidget.js
@@ -0,0 +1,13 @@
+import Cart from "./cart.svg"
+
+const CartWidget = () => {
+    return(
+
+        <div>
+            <img src={Cart} width="30" height="25" alt="cart-compra"/>
+            0
+        </div>
+    )
+
+}
+export default CartWidget
